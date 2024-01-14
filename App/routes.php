8766858330ee42329c382cb10d13c85f11fe186c
Aleksey Batuev <soks145gmail.com>
2024-01-14 19:58:26 +0500

    Задание 3. Работа с БД. Модели
    Signed-off-by: Aleksey Batuev <soks145gmail.com>

--- App/routes.php
+++ App/routes.php
@@ -5,4 +5,6 @@
     Router::addRoute(new Route('page/{id}/create', 'PageController@create', Route::METHOD_GET));
     Router::addRoute(new Route('page/{id}/update', 'PageController@update', Route::METHOD_GET));
     Router::addRoute(new Route('page/{id}', 'PageController@page', Route::METHOD_GET));
+    Router::addRoute(new Route('user/{id}', 'UserController@getById', Route::METHOD_GET));
+    Router::addRoute(new Route('user', 'UserController@index', Route::METHOD_GET));
 
